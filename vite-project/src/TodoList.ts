0cf0039cd6e09f97addfac7a3e1ca7d1eac3f000
AbelCastilla-04 <143293264+AbelCastilla-04@users.noreply.github.com>
2026-02-11 17:07:00 -0600

    major changes

--- vite-project/src/TodoList.ts
+++ vite-project/src/TodoList.ts
@@ -4,42 +4,34 @@ export interface Todo {
   completed: boolean;
 }
 
-class TodoList {
-    constructor() {
-        // TODO: What type should this array hold?
-        this.todos = [];
-    }
+export class TodoList {
+    private todo: Todo[] = [];
 
-    // TODO: Add parameter type and return type
-    addTodo(text) {
-        const todo = {
-            id: crypto.randomUUID(),
-            text: text,
-            completed: false
-        };
-        this.todos.push(todo);
-        return todo;
-    }
+  addTodo(text: string): Todo {
+    const todo: Todo = {
+      id: crypto.randomUUID(),
+      text,
+      completed: false,
+    };
+    this.todos.push(todo);
+    return todo;
+  }
 
-    // TODO: Add parameter type and return type
-    removeTodo(id) {
-        this.todos = this.todos.filter(todo => todo.id !== id);
-    }
+  removeTodo(id: string): void {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
 
-    // TODO: Add parameter type and return type
-    toggleTodo(id) {
-        this.todos = this.todos.map(todo => 
-            todo.id === id ? { ...todo, completed: !todo.completed } : todo
-        );
-    }
+  toggleTodo(id: string): void {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+  }
 
-    // TODO: Add return type
-    listTodos() {
-        return this.todos;
-    }
+  listTodos(): Todo[] {
+    return this.todos;
+  }
 
-    // TODO: Add return type
-    getTodoCount() {
-        return this.todos.length;
-    }
+  getTodoCount(): number {
+    return this.todos.length;
+  }
 }
